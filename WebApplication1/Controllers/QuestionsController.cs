2ba3e75fba705f4fb82824ec40658e1a1e22f00b
dominicbillena06-prog <dominicbillena06@gmail.com>
2026-09-11 22:25:04 +0800

    Answer question 1

---
+++ WebApplication1/Controllers/QuestionsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IT_ELECTIVE_2_BSIT31A3_PREFINAL_EXAM_Billena_Dominic.Models;
+
+namespace IT_ELECTIVE_2_BSIT31A3_PREFINAL_EXAM_Billena_Dominic.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var questions = new List<Question>
+            {
+                new Question
+                {
+                    Number = 1,
+                    Text = "What is the main problem solved by using a database instead of an in-memory collection?",
+                    Choices = new List<string>
+                    {
+                        "A. It makes C# code shorter",
+                        "B. It prevents the application from restarting",
+                        "C. It allows data to persist after the application stops",
+                        "D. It removes the need for MVC"
+                    },
+                    Answer = "C"
+                }
+            };
+
+            return View(questions);
+        }
+    }
+}
---
+++ WebApplication1/Models/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_ELECTIVE_2_BSIT31A3_PREFINAL_EXAM_Billena_Dominic.Models
+{
+    public class Question
+    {
+        public int Number { get; set; }
+        public string Text { get; set; } = "";
+        public List<string> Choices { get; set; } = new();
+        public string Answer { get; set; } = "";
+    }
+}
